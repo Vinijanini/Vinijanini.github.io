929b40555d4b97f96471e253d1482ff11570eeab
Vinícius Janini <89104824+Vinijanini@users.noreply.github.com>
2024-05-22 23:58:42 -0300

    Add files via upload

---
+++ cod.js
@@ -0,0 +1,16 @@
+var formacao = formacao = document.getElementById('divisao_formacao')
+formacao.addEventListener('mouseenter', amplia_formacao)
+formacao.addEventListener('mousemove', amplia_formacao)
+formacao.addEventListener('mouseout', desamplia_formacao)
+
+function amplia_formacao() {
+    let formacao = document.getElementById('divisao_formacao')
+    formacao.style.scale = '1.20'
+    titulo_formacao.style.position = 'relative'
+    titulo_formacao.style.bottom = '20px'
+}
+function desamplia_formacao() {
+    let formacao = document.getElementById('divisao_formacao')
+    formacao.style.scale = '1'
+    titulo_formacao.style.bottom = '0px'
+}
