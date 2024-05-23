93059387dba434844811ee735b2e8a1fd0ffb42f
Vinícius Janini <89104824+Vinijanini@users.noreply.github.com>
2024-05-23 00:13:47 -0300

    Add files via upload

--- cod.js
+++ cod.js
@@ -1,7 +1,9 @@
 var formacao = formacao = document.getElementById('divisao_formacao')
 formacao.addEventListener('mouseenter', amplia_formacao)
 formacao.addEventListener('mousemove', amplia_formacao)
+formacao.addEventListener('touchstart', amplia_formacao)
 formacao.addEventListener('mouseout', desamplia_formacao)
+formacao.addEventListener('touchend', desamplia_formacao)
 
 function amplia_formacao() {
     let formacao = document.getElementById('divisao_formacao')
